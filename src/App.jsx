77eccbc96c78290c7668674105d7486a0be03d12
Sam Steven <samfnatixsteven4@gmail.com>
2026-02-11 01:43:19 +0530

    added authentication service

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  console.log(import.meta.env.VITE_APPWRITE)
+  return (
+    <>
+      <h1>hello sam</h1>
+    </>
+  )
+}
+
+export default App
